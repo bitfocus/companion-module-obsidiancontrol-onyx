07511c02a19fd6b22d4f9dba1bc74931c33fed79
Zachary Klosko <zklosko@hopecentral.com>
2026-04-02 09:41:01 -0400

    feat: added upgrade script for actionId and feedbackId changes

--- src/upgrades.js
+++ src/upgrades.js
@@ -3,11 +3,69 @@ export const upgradeScripts = [
 	 * Place your upgrade scripts here
 	 * Remember that once it has been added it cannot be removed!
 	 */
-	// function (context, props) {
-	// 	return {
-	// 		updatedConfig: null,
-	// 		updatedActions: [],
-	// 		updatedFeedbacks: [],
-	// 	}
-	// },
+	function updateIDs (context, props) {
+		const result = {
+		updatedConfig: null,
+		updatedSecrets: null,
+		updatedActions: [],
+		updatedFeedbacks: [],
+		}
+
+		for (const action of props.actions) {
+			// Update action IDs
+			switch(action.actionId) {
+				case 'release_all_overrides':
+					action.actionId = 'releaseOverides'
+					result.updatedActions.push(action)
+					break
+				case 'release_all_cl':
+					action.actionId = 'releaseCuelists'
+					result.updatedActions.push(action)
+					break
+				case 'release_all_cl_df':
+					action.actionId = 'releaseCuelistsDimFirst'
+					result.updatedActions.push(action)
+					break
+				case 'release_all_cl_or':
+					action.actionId = 'releaseAll'
+					result.updatedActions.push(action)
+					break
+				case 'clearclear':
+					action.actionId = 'clearProgrammer'
+					result.updatedActions.push(action)
+					break
+				case 'go_list_cue':
+					action.actionId = 'goCuelist'
+					result.updatedActions.push(action)
+					break
+				case 'pause_cuelist':
+					action.actionId = 'pauseCuelist'
+					result.updatedActions.push(action)
+					break
+				case 'release_cl':
+					action.actionId = 'releaseCuelist'
+					result.updatedActions.push(action)
+					break
+				case 'go_cue':
+					action.actionId = 'goToCue'
+					result.updatedActions.push(action)
+					break
+				case 'go_schedule':
+					action.actionId = 'goSchedule'
+					result.updatedActions.push(action)
+					break
+				default:
+					break
+			}
+		}
+
+		for (const feedback of props.feedbacks) {
+			if (feedback.feedbackId = 'cuelist_active') {
+				feedback.feedbackId = 'ActiveCuelist'
+				result.updatedFeedbacks.push(feedback)
+			}
+		}
+
+		return result
+	},
 ]
